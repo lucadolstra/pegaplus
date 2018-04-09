c6ba97c52063e5236f20d018142558b4a0fd8a03
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-04-09 15:08:44 +0200

    Added new text

--- src/restaurant/Application.java
+++ src/restaurant/Application.java
@@ -3,5 +3,6 @@ package restaurant;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Shami's add");
     }
 }
