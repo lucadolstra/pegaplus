1c5c4ca4cb7716b757f898255b9522825aca7c47
KevinTTT2310 <38212230+KevinTTT2310@users.noreply.github.com>
2018-04-09 15:15:16 +0200

    Fixed

--- src/restaurant/Application.java
+++ src/restaurant/Application.java
@@ -5,10 +5,10 @@ import java.sql.SQLOutput;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
-<<<<<<< HEAD
+
         System.out.println("Have a nice day");
-=======
+
         System.out.println("Shami's add");
->>>>>>> c6ba97c52063e5236f20d018142558b4a0fd8a03
+
     }
 }
