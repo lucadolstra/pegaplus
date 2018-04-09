aba190603e5083adf04bf84d636fac63f9d732df
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-04-09 15:08:50 +0200

    Test Carlijn

--- src/restaurant/Application.java
+++ src/restaurant/Application.java
@@ -1,7 +1,10 @@
 package restaurant;
 
+import java.sql.SQLOutput;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Have a nice day");
     }
 }
